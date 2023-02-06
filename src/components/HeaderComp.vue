2667516cec41df9215d045a2e0b24e5ff6d465d0
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-02-06 16:45:30 +0100

    file HeaderComp

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="">
+    
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'HeaderComp',
+ 
+  data() {
+    return {
+      inputText: '',
+    }
+  },
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.header{
+  height: 15%;
+  background-color: black;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+
+  img{
+    width: 15%;
+  }
+
+  .inputButton{
+    display: flex;
+    
+
+  button{
+    padding: 8px;
+    
+  }
+
+  }
+
+}
+
+</style>
