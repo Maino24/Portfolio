87e9d404473ebe22f4ebf7a6a85ae7ff0f3d88b9
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-02-10 18:32:12 +0100

    nav

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="background">
         <div class="containerNP">
+            <ul class="nav animated bounceInDown">
+                <li><a href="#AboutMe">About Me</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#contacts">Contacts</a></li>
+            </ul>
+        
             <h1 class="name animated bounceInDown">Gabriele Mainetti</h1>
             <h1 class="profession animated bounceInDown">Junior Full Stack Web Developer</h1>
         </div>
         
-        <!-- <ul>
-            <li><button>About Me</button></li>
-            <li><button>Skills</button></li>
-            <li><button>Projects</button></li>
-            <li><button>Resume</button></li>
-            <li><button>Contacts</button></li>
         
-        </ul>-->
         
     </div>
 </template>
@@ -26,37 +27,45 @@ export default {
 <style lang="scss">
 .background{
   background-color: blue;
-  width: 100vw;
+  //width: 100vw;
   height: 100vh;
 }
+
 .containerNP{
-    padding-top: 15%;
     width: 70%;
     margin: auto;
-    
-    .name{
+
+    .nav{
+    display: flex;
+    margin: auto;
+    list-style: none;
+    justify-content: space-between;
+    padding-top: 5%;
+    font-size: 2rem;
     color: white;
-    font-size: 5rem;
+    a{
+        text-decoration: none;
+        color: white;
+    }
+    a:hover{
+        font-size: 2.5rem;
+        border-bottom: 2px solid black;
+    }
+    }
+
+    .name{
+        padding-top: 15%;
+        color: white;
+        font-size: 5rem;
+        box-shadow: 10px;
     }
 
     .profession{
         color: white;
         font-size: 4rem;
         }
-}   
-
-nav{
-    display: flex;
-   /* ul{
-        display: flex;
-        
-        li{
-        list-style: none;
-        margin: auto;
-        justify-content: space-around;
-        }
-    }*/
-    
+   
 }
+
 
 </style>
