747b8cfa5996f90daa4b8fe0247518597bd6a027
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-02-10 18:32:01 +0100

    ContactsComp

---
+++ src/components/ContactsComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="backgroundC">
+        <a href="" name="contacts"></a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactsComp'
+}
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.backgroundC{
+  border-top: 2px solid black;
+  background-color: blue;
+  //width: 100vw;
+  height: 100vh;
+  .containerAM{
+    padding-top: 2%;
+    width: 70%;
+    margin: auto;
+    color: white;
+  }
+}
+</style>
