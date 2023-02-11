2411a97ecdd47ed659f2911405cdc11ea73cf26d
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-02-11 18:04:23 +0100

    ExperienceComp

--- src/components/ExperienceComp.vue
+++ src/components/ExperienceComp.vue
@@ -2,7 +2,30 @@
     <div class="backgroundE">
       <div class="containerE">
         <h1 class="title">Esperienza</h1>
-
+        <div class="firstExperience">
+          <div class="d-flex">
+            <h3>Jr Full Stack Web Developer Trainee</h3>
+            <img src="../assets/logoBoolean.png" alt="">
+          </div>
+          <h4>Boolean Careers</h4>
+          <h4>Settembre 2022 - Marzo 2023 | 6 mesi</h4>
+          <p class="parag">Sviluppo di progetti ispirati a web app esistenti come: Netflix,
+              Whatsapp Web, Spotify, Zoom, Zalando ... .
+              <br>
+              Tutti i progetti svolti avevano una scadenza e questo mi ha permesso
+              di avvicinarmi a ciò che avviene effettivamente nel mondo del lavoro.
+          </p>
+        </div>
+        <div class="secondExperience">
+          <div class="d-flex">
+            <h3>Impiegato Contabile</h3>
+          </div>
+          <h4>Midac S.r.l.</h4>
+          <h4>Agosto 2022 - agosto 2023 | 1 anno</h4>
+          <p class="parag">Impiegato contabile presso studio commercialista. Gestione
+            contabilità, mail, chiamate per un gruppo di società
+          </p>
+        </div>
       </div>
     </div>
 </template>
@@ -32,6 +55,20 @@ export default {
     .title{
       font-size: 4rem;
     }
+    .firstExperience{
+      border-bottom: 2px solid white;
+      padding: 5% 0;
+      div{
+        img{
+        width: 60px;
+        padding-left: 10px;
+        }
+      }      
+    }
+    .secondExperience{
+        //border-bottom: 2px solid white;
+        padding-top: 5%;
+      }
   }
 }
 </style>
