04dffce3ff71eaa49f1ef4c1281e995446535717
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-02-13 11:17:11 +0100

    header img

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -8,9 +8,33 @@
                 <li><a href="#resume">Resume</a></li>
                 <li><a href="#contacts">Contacts</a></li>
             </ul>
-        
-            <h1 class="name animated bounceInDown">I'm Gabriele Mainetti</h1>
-            <h1 class="profession animated bounceInDown">Junior Full Stack Web Developer</h1>
+            <div class="nameImg">
+                <div class="containerName">
+                    <h1 class="name animated bounceInDown">I'm Gabriele Mainetti</h1>
+                    <h1 class="profession animated bounceInDown">Junior Full Stack Web Developer</h1>
+                </div>
+                <div class="containerImg">
+                    <img src="../assets/pc.webp" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="contacts">
+            <div class="contactsFirst">
+                <li>
+                    <i class="fa-solid fa-house"></i><span>Sondrio, Lombardia Italia</span>
+                </li>
+                <li>
+                    <i class="fa-solid fa-envelope"></i><span>mg02.gabriele@gmail.com</span>
+                </li>
+            </div>
+            <div class="contactsSecond">
+                <li>
+                    <a href="https://www.linkedin.com/in/gabriele-mainetti/"><i class="fa-brands fa-linkedin"></i>https://www.linkedin.com/in/gabriele-mainetti</a>
+                </li>
+                <li>
+                    <a href="https://github.com/Maino24"><i class="fa-brands fa-github"></i>https://github.com/Maino24</a>
+                </li>
+            </div>
         </div>
         
         
@@ -52,19 +76,77 @@ export default {
         border-bottom: 2px solid black;
     }
     }
+    .nameImg{
+        display: flex;
+        //margin: auto;
+        justify-content: space-between;
+        padding-top: 7%;
+        .containerName{
+            flex-basis: 70%;
+            .name{
+            //padding-top: 15%;
+            color: white;
+            font-size: 5rem;
+            box-shadow: 10px;
+            }
 
-    .name{
-        padding-top: 15%;
-        color: white;
-        font-size: 5rem;
-        box-shadow: 10px;
-    }
-
-    .profession{
-        color: white;
-        font-size: 4rem;
+            .profession{
+                color: white;
+                font-size: 3rem;
+            }
         }
-   
+        .containerImg{
+            flex-basis: 30%;
+            img{
+                width: 90%
+            }
+        }
+       
+    }
+    
+}
+    .contacts{
+        width: 70%;
+        margin: auto;
+        //border: 1.5px solid white;
+        display: flex;
+        justify-content: space-between;
+        padding-top: 5%;
+        .contactsFirst{
+            flex-basis: 40%;
+            color: white;
+            li{
+            list-style: none;
+            //padding-top: 5%;
+            font-size: 1.5rem;
+            padding-top: 10%;
+                i{
+                    margin-right: 3%;
+                    color: white;
+                }
+        
+            }
+        }
+        .contactsSecond{
+            flex-basis: 40%;
+            color: white;
+            color: white;
+                li{
+                list-style: none;
+                //padding-top: 5%;
+                font-size: 1.5rem;
+                padding-top: 10%;
+                a{
+                    text-decoration: none;
+                    color: white;
+                    i{
+                        margin-right: 3%;
+                        color: white;
+                    }
+                }
+                }
+
+        }
 }
 
 
