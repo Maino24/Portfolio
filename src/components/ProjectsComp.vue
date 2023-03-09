f546577f336c57a137f736d85a0cd7b97c0c0f47
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-03-09 09:25:10 +0100

    projects tablet

--- src/components/ProjectsComp.vue
+++ src/components/ProjectsComp.vue
@@ -332,4 +332,112 @@ export default {
   }
 }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@media screen and (max-width:820px){
+.backgroundP{
+  padding-top: 2px solid black;
+  background-color: rgb(5, 5, 124);
+  width: 100%;
+  height: 100vh;
+  padding-bottom: 500px;
+  .containerP{
+    width: 80%;
+    margin: auto;
+    padding: 2% 0;
+    
+    .title{
+        font-size: 4rem;
+        color: white;
+        padding-bottom: 5%;
+    }
+    .card{
+        width: 47%;
+        padding: 1%;
+        
+        img{
+            height: 100px;
+        }
+        .projectsTitles{
+            text-align: center;
+            font-weight: bold;
+            font-size: 0.8rem;
+            padding-top: 10px;
+        }
+        .card-body{
+            display: flex;
+            justify-content: center;
+            padding-top: 2px;
+        }
+        i{
+            color:black;
+        }
+        .button {
+            height: 40px;
+            padding: 0 3em;
+            border: none;
+            border-radius: 5px;
+            font-weight: bold;
+            text-transform: uppercase;
+            color: black;
+            transition: all 1000ms;
+            font-size: 15px;
+            position: relative;
+            overflow: hidden;
+            outline: 2px solid black;
+            }
+
+            button:hover {
+            color: black;
+            transform: scale(1.1);
+            outline: 2px solid #4862c1;
+            box-shadow: 4px 5px 17px -4px #4862c1;
+            }
+
+            button::before {
+            content: "";
+            position: absolute;
+            left: -50px;
+            top: 0;
+            width: 0;
+            height: 100%;
+            background-color: #4862c1;
+            transform: skewX(45deg);
+            z-index: -1;
+            transition: width 1000ms;
+            }
+
+            button:hover::before {
+            width: 250%;
+            }
+
+        }
+    
+    }
+}
+
+}
 </style>
